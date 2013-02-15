d9f2dcebb17b1e1b6d9f625da044eaab014c4a8c
Jason Rudy <jcrudy@gmail.com>
2013-02-15 11:48:23 -0800

    now?

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Distutils import build_ext, install
 import numpy
 from Cython.Build import cythonize
 
@@ -8,7 +8,7 @@ setup(
     name='choldate',
     version='0.1.0',
     py_modules=['choldate.test'],
-    cmdclass = {'build_ext': build_ext},
+    cmdclass = {'build_ext': build_ext,'install': install},
     ext_modules = cythonize([Extension("choldate", ["choldate/choldate.pyx"],include_dirs = [numpy.get_include()])
     ]), requires=['numpy']
 
