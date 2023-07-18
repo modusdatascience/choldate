d37246f4fc1775f11b84d42b5ceba08e6392d285
Alexey C <54956904+ColdTeapot273K@users.noreply.github.com>
2023-07-18 11:18:46 +0500

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# import numpy
-# from Cython.Build import cythonize
 import subprocess
 import sys
 
@@ -23,34 +21,12 @@ except ImportError:
 
 ext_modules = cythonize([Extension("choldate._choldate", ["choldate/_choldate.pyx"], include_dirs=[get_include()])])
 
-# ext_modules = (
-#     cythonize(
-#         module_list=[
-#             setuptools.Extension(
-#                 "*",
-#                 sources=["**/*.pyx"],
-#                 include_dirs=[get_include()],
-#             )
-#         ],
-#         compiler_directives={
-#             "language_level": 3,
-#             "binding": True,
-#             "embedsignature": True,
-#         },
-#     ),
-# )
-
 setup(
     name="choldate",
     version="0.1.0",
-    # packages=["choldate", "choldate.test"],
     packages=setuptools.find_packages(),
     include_package_data=True,
-    # ext_modules=cythonize(extensions),
     ext_modules=ext_modules,
-    # include_dirs=[numpy.get_include()],
-    # install_requires=["numpy", "cython"],
-    # install_requires=(base_packages := [f"numpy>={numpy_version}", "cython"]),
     install_requires=(base_packages := [f"numpy>={numpy_version}"]),
     zip_safe=False,
 )
